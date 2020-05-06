f212e46dd8b594df2ec314c48febdd4fbbadde40
francois-charlier <64131518+francois-charlier@users.noreply.github.com>
2020-05-06 19:16:20 +0200

    Add files via upload

--- Projet_SQL/js/projet.js
+++ Projet_SQL/js/projet.js
@@ -39,6 +39,8 @@ function goToCateg() {
     getElemId('sondage').hidden = true;
     getElemId('profil').hidden = true;
 
+    getElemId('sondageBienCree').hidden= true;
+
     estAnonyme();
 }
 
@@ -241,14 +243,132 @@ function nombresReponse(param) {
 }
 
 function creationSondage(formulaire) {
-    let question = formulaire.question.value;
-    let categ = formulaire.categorie.value;
-    let rep1 = formulaire.rep1.value;
-    let rep2 = formulaire.rep2.value;
-    let rep3 = formulaire.rep3.value;
-    let rep4 = formulaire.rep4.value;
-    let rep5 = formulaire.rep5.value;
-    let rep6 = formulaire.rep6.value;
+
+    if (getElemId('nbreponse').value == 3) {
+        let idUser = idConnecté;
+        let question = formulaire.question.value;
+        let categ = formulaire.categorie.value;
+        let rep1 = formulaire.rep1.value;
+        let rep2 = formulaire.rep2.value;
+        let rep3 = formulaire.rep3.value;
+        let rep4 = '0';
+        let rep5 = '0';
+        let rep6 = '0';
+
+        let xhr = new XMLHttpRequest;
+        xhr.open('get', 'http://localhost/createSondage?userId=' + idUser + '&question=' + question + '&categId=' + categ + '&rep1=' + rep1 + '&rep2=' + rep2 + '&rep3=' + rep3 + '&rep4='+ rep4 + '&rep5=' + rep5 + '&rep6=' + rep6, true);
+        xhr.send();
+
+        formulaire.question.value = "";
+        getElemId('selected').selected=true;
+        nombresReponse(2);
+        formulaire.rep1.value = "";
+        formulaire.rep2.value = "";
+        formulaire.rep3.value = "";
+        getElemId('sondageBienCree').hidden= false;
+    }
+
+    else if (getElemId('nbreponse').value == 4) {
+        let idUser = idConnecté;
+        let question = formulaire.question.value;
+        let categ = formulaire.categorie.value;
+        let rep1 = formulaire.rep1.value;
+        let rep2 = formulaire.rep2.value;
+        let rep3 = formulaire.rep3.value;
+        let rep4 = formulaire.rep4.value;
+        let rep5 = '0';
+        let rep6 = '0';
+
+        let xhr = new XMLHttpRequest;
+        xhr.open('get', 'http://localhost/createSondage?userId=' + idUser + '&question=' + question + '&categId=' + categ + '&rep1=' + rep1 + '&rep2=' + rep2 + '&rep3=' + rep3 + '&rep4='+ rep4 + '&rep5=' + rep5 + '&rep6=' + rep6, true);
+        xhr.send();
+
+        formulaire.question.value = "";
+        getElemId('selected').selected=true;
+        nombresReponse(2);
+        formulaire.rep1.value = "";
+        formulaire.rep2.value = "";
+        formulaire.rep3.value = "";
+        formulaire.rep4.value = "";
+        getElemId('sondageBienCree').hidden= false;
+    }
+
+    else if (getElemId('nbreponse').value == 5) {
+        let idUser = idConnecté;
+        let question = formulaire.question.value;
+        let categ = formulaire.categorie.value;
+        let rep1 = formulaire.rep1.value;
+        let rep2 = formulaire.rep2.value;
+        let rep3 = formulaire.rep3.value;
+        let rep4 = formulaire.rep4.value;
+        let rep5 = formulaire.rep5.value;
+        let rep6 = '0';
+
+        let xhr = new XMLHttpRequest;
+        xhr.open('get', 'http://localhost/createSondage?userId=' + idUser + '&question=' + question + '&categId=' + categ + '&rep1=' + rep1 + '&rep2=' + rep2 + '&rep3=' + rep3 + '&rep4='+ rep4 + '&rep5=' + rep5 + '&rep6=' + rep6, true);
+        xhr.send();
+
+        formulaire.question.value = "";
+        getElemId('selected').selected=true;
+        nombresReponse(2);
+        formulaire.rep1.value = "";
+        formulaire.rep2.value = "";
+        formulaire.rep3.value = "";
+        formulaire.rep4.value = "";
+        formulaire.rep5.value = "";
+        getElemId('sondageBienCree').hidden= false;
+    }
+
+    else if (getElemId('nbreponse').value == 6) {
+        let idUser = idConnecté;
+        let question = formulaire.question.value;
+        let categ = formulaire.categorie.value;
+        let rep1 = formulaire.rep1.value;
+        let rep2 = formulaire.rep2.value;
+        let rep3 = formulaire.rep3.value;
+        let rep4 = formulaire.rep4.value;
+        let rep5 = formulaire.rep5.value;
+        let rep6 = formulaire.rep6.value;
+
+        let xhr = new XMLHttpRequest;
+        xhr.open('get', 'http://localhost/createSondage?userId=' + idUser + '&question=' + question + '&categId=' + categ + '&rep1=' + rep1 + '&rep2=' + rep2 + '&rep3=' + rep3 + '&rep4='+ rep4 + '&rep5=' + rep5 + '&rep6=' + rep6, true);
+        xhr.send();
+
+        formulaire.question.value = "";
+        getElemId('selected').selected=true;
+        nombresReponse(2);
+        formulaire.rep1.value = "";
+        formulaire.rep2.value = "";
+        formulaire.rep3.value = "";
+        formulaire.rep4.value = "";
+        formulaire.rep5.value = "";
+        formulaire.rep6.value = "";
+        getElemId('sondageBienCree').hidden= false;
+    }
+
+    else {
+        let idUser = idConnecté;
+        let question = formulaire.question.value;
+        let categ = formulaire.categorie.value;
+        let rep1 = formulaire.rep1.value;
+        let rep2 = formulaire.rep2.value;
+        let rep3 = '0';
+        let rep4 = '0';
+        let rep5 = '0';
+        let rep6 = '0';
+
+        let xhr = new XMLHttpRequest;
+        xhr.open('get', 'http://localhost/createSondage?userId=' + idUser + '&question=' + question + '&categId=' + categ + '&rep1=' + rep1 + '&rep2=' + rep2 + '&rep3=' + rep3 + '&rep4='+ rep4 + '&rep5=' + rep5 + '&rep6=' + rep6, true);
+        xhr.send();
+
+        formulaire.question.value = "";
+        getElemId('selected').selected=true;
+        nombresReponse(2);
+        formulaire.rep1.value = "";
+        formulaire.rep2.value = "";
+        getElemId('sondageBienCree').hidden= false;
+    }
+    return false;
 }
 
 
