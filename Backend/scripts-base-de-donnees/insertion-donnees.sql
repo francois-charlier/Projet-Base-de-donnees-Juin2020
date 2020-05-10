a2673670274ece5d2cdcb797cb5c977161186b3d
MaximeLitsEphec <64195285+MaximeLitsEphec@users.noreply.github.com>
2020-05-10 16:09:32 +0200

    Update insertion-donnees.sql

--- Backend/scripts-base-de-donnees/insertion-donnees.sql
+++ Backend/scripts-base-de-donnees/insertion-donnees.sql
@@ -5,4 +5,4 @@ INSERT INTO "DBA"."categories" ("categId","categName") VALUES('J','Jeux vidéos'
 INSERT INTO "DBA"."categories" ("categId","categName") VALUES('N','Nourriture');
 INSERT INTO "DBA"."categories" ("categId","categName") VALUES('P','Politique');
 INSERT INTO "DBA"."categories" ("categId","categName") VALUES('S','Social');
-
+INSERT INTO "DBA"."users" (userPseudo, userPassword, userNom, userPrenom, userDate, userMail, userSexe) VALUES  ('admin','adminsql', 'Lits', 'Maxime', '2020-10-20', 'admin@gmail.com', 'A');
